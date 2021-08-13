cb6ecdc11baf3bcf6852761951776693db526865
chinmay <chinmaykulkarni0701@gmail.com>
2021-08-13 07:26:54 -0400

    Bio

--- lib/screens/buttonPages/Bio.dart
+++ lib/screens/buttonPages/Bio.dart
@@ -373,9 +373,9 @@ class _BioState extends State<Bio> {
                       decoration: displayUrl == null
                           ? BoxDecoration(
                               image: DecorationImage(
-                                image: widget.displayPicture != null
+                                image: displayPicture != null
                                     ? CachedNetworkImageProvider(
-                                        widget.displayPicture)
+                                        displayPicture)
                                     // ? NetworkImage(widget.displayPicture)
                                     : AssetImage('assets/images/person.png'),
                               ),
@@ -456,12 +456,12 @@ class _BioState extends State<Bio> {
                   const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
               child: TextFormField(
                 decoration: InputDecoration(  hintText: data['bio'] != null
-        ? data['bio']
-        : 'Bio', labelText: 'Bio'),
-                controller: bioTextEditingControler,
+                      ? data['bio']
+                      : 'Bio', labelText: 'Bio'),
+         //       controller: bioTextEditingControler,
                 autofocus: true,
                 maxLines: null,
-                initialValue: widget.bio,
+                initialValue: data['bio'],
                 onChanged: (value) {
                   setState(() {
 
@@ -501,7 +501,9 @@ class _BioState extends State<Bio> {
               padding: const EdgeInsets.all(15),
               child: Column(
                 children: [
-                  TextField(
+                  TextFormField(
+                    keyboardType: TextInputType.url,
+                    initialValue: data['instagram'],
                     onChanged: (value) {
                       setState(() {
                         instagram = value;
@@ -513,7 +515,9 @@ class _BioState extends State<Bio> {
                             ? data['instagram']
                             : 'https://instagram.com/john_snow'),
                   ),
-                  TextField(
+                  TextFormField(
+                    keyboardType: TextInputType.url,
+                    initialValue: data['twitter'],
                     onChanged: (value) {
                       setState(() {
                         twitter = value;
@@ -524,7 +528,9 @@ class _BioState extends State<Bio> {
                         hintText:
                             '${data['twitter'] == null ? 'https://twitter.com/@john_snow' : data['twitter']} '),
                   ),
-                  TextField(
+                  TextFormField(
+
+                    initialValue: data['linkedin'] ,
                     onChanged: (value) {
                       setState(() {
                         linkedin = value;
@@ -535,7 +541,9 @@ class _BioState extends State<Bio> {
                         hintText:
                             '${data['linkedin'] != null ? data['linkedin'] : 'https://linkedin/com/john_snow'}'),
                   ),
-                  TextField(
+                  TextFormField(
+                    keyboardType: TextInputType.url,
+                    initialValue:  data['website'],
                     onChanged: (value) {
                       setState(() {
                         website = value;
