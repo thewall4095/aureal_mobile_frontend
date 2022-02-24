6df94c404666307dba8dc017d78fe76a3f8de781
shubham upadhyay <shubhamupadhyay@shubhams-MacBook-Air.local>
2022-02-24 18:46:45 +0530

    Changes after android release

--- lib/screens/Profiles/publicUserProfile.dart
+++ lib/screens/Profiles/publicUserProfile.dart
@@ -1,21 +1,16 @@
 import 'dart:convert';
-import 'package:auditory/Services/DurationCalculator.dart';
-import 'package:auditory/Services/Interceptor.dart' as postreq;
-import 'package:auditory/screens/buttonPages/search.dart';
-import 'package:auditory/utilities/Share.dart';
 import 'dart:io';
-import 'package:html/parser.dart';
-import 'package:assets_audio_player/assets_audio_player.dart';
+
+import 'package:auditory/Services/DurationCalculator.dart';
 import 'package:auditory/Services/HiveOperations.dart';
+import 'package:auditory/Services/Interceptor.dart' as postreq;
 import 'package:auditory/Services/LaunchUrl.dart';
 import 'package:auditory/screens/Onboarding/HiveDetails.dart';
-import 'package:auditory/screens/Player/Player.dart';
-import 'package:auditory/screens/Player/PlayerElements/Seekbar.dart';
-import 'package:auditory/screens/Player/VideoPlayer.dart';
 import 'package:auditory/screens/Profiles/EpisodeView.dart';
+import 'package:auditory/screens/buttonPages/search.dart';
+import 'package:auditory/utilities/Share.dart';
 import 'package:auditory/utilities/SizeConfig.dart';
 import 'package:auditory/utilities/constants.dart';
-import 'package:auditory/utilities/getRoomDetails.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
@@ -24,16 +19,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 import 'package:jitsi_meet/jitsi_meet.dart';
-import 'package:line_icons/line_icon.dart';
-import 'package:line_icons/line_icons.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../PlayerState.dart';
-import '../Clips.dart';
 import 'Comments.dart';
 import 'PodcastView.dart';
 
@@ -1627,10 +1620,10 @@ class _FollowersState extends State<Followers> {
                                                   3.5,
                                           fontWeight: FontWeight.bold),
                                     ),
-                                    v['fullname'] == null
+                                    v['hive_username'] == null
                                         ? SizedBox()
                                         : Text(
-                                            "${v['fullname']}",
+                                            "${v['hive_username']}",
                                             textScaleFactor: 1.0,
                                             overflow: TextOverflow.ellipsis,
                                             maxLines: 1,
