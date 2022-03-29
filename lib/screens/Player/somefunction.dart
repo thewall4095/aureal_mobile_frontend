687fb1d3858287443799be80fd601a4476b99671
shubham upadhyay <shubhamupadhyay@shubhams-MacBook-Air.local>
2022-03-29 19:51:51 +0530

    merged with master

---
+++ lib/screens/Player/somefunction.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SomeFunction extends StatefulWidget {
+  @override
+  _SomeFunctionState createState() => _SomeFunctionState();
+}
+
+class _SomeFunctionState extends State<SomeFunction> {
+  void checkVideoExtention() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
